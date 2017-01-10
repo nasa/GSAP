60e5a1d16f106bb5855d3ff8b8118f30f0b1f8b5
Chris Teubert <christopher.a.teubert@nasa.gov>
2017-01-10 14:56:41 -0800

    Updated Example

--- example/progMain.cpp
+++ example/progMain.cpp
@@ -1,41 +1,59 @@
-/**  Start point for the Generic Architecture
- *   @ingroup   GPIC++
- *   @ingroup   Framework
- *
- *   @brief     Example starting point for the generic architecture. Specifies prognosers, communicators, starts ProgManager
- *
- *   @author    Chris Teubert
- *   @version   0.1.0
- *
- *   @pre       Prognostic Configuration File and Prognoster Configuration Files
- *
- *      Contact: Chris Teubert (Christopher.a.teubert@nasa.gov)
- *      Created: March 17, 2016
- *
- *   @copyright Copyright (c) 2013-2016 United States Government as represented by
- *     the Administrator of the National Aeronautics and Space Administration.
- *     All Rights Reserved.
- */
+//
+//  progMain.cpp
+//  Example
+//
+//  Created by Teubert, Christopher (ARC-TI)[SGT, INC] on 5/4/16.
+//  Copyright © 2016 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+//
 
 #include "ProgManager.h"
-#include "ThreadSafeLog.h"
 
-const std::string MODULE_NAME = "Verification Test-Mac";
+#include "PrognoserFactory.h"
+#include "ModelBasedPrognoser.h"
 
-int main(int argc, const char * argv[]) {
-    std::string configFile("/Users/cteubert/Documents/GSAP/GSAP_Code/cpp/Test/Verification Tests/cfg/prog_mac.cfg");
-    if (argc >= 2) {
-        configFile = argv[1];
-    }
-    
-    const Log & log(Log::Instance());
-    //log.setVerbosity(LOG_TRACE);
-    
-    ProgManager PM = ProgManager(configFile);
-    
+#include "CommunicatorFactory.h"
+#include "RandomCommunicator.h"
+#include "RecorderCommunicator.h"
+#include "PlaybackCommunicator.h"
+#include "ConfigMap.h"
+#include "ModelFactory.h"
+#include "PrognosticsModelFactory.h"
+#include "ObserverFactory.h"
+#include "PredictorFactory.h"
+#include "UnscentedKalmanFilter.h"
+#include "MonteCarloPredictor.h"
+#include "Battery.h"
+
+using namespace PCOE;
+
+int main() {
+    // Specify configuration file directories
+    ConfigMap::addSearchPath("../example/cfg/");
+
+    // Specify Prognosers
+    PrognoserFactory & prognoserFactory = PrognoserFactory::instance();
+    prognoserFactory.Register("modelBasedPrognoser", PrognoserFactory::Create<ModelBasedPrognoser>);
+
+    // Specify Communicators
+    CommunicatorFactory & commFactory = CommunicatorFactory::instance();
+    commFactory.Register("recorder",  CommunicatorFactory::Create<RecorderCommunicator>);
+    commFactory.Register("playback",  CommunicatorFactory::Create<PlaybackCommunicator>);
+
+    // Register battery model
+    ModelFactory & pModelFactory = ModelFactory::instance();
+    PrognosticsModelFactory & pProgModelFactory = PrognosticsModelFactory::instance();
+    pModelFactory.Register("Battery", ModelFactory::Create<Battery>);
+    pProgModelFactory.Register("Battery", PrognosticsModelFactory::Create<Battery>);
+
+    // Register UKF
+    ObserverFactory & pObserverFactory = ObserverFactory::instance();
+    pObserverFactory.Register("UKF", ObserverFactory::Create<UnscentedKalmanFilter>);
+
+    // Register MonteCarloPredictor
+    PredictorFactory & pPredictorFactory = PredictorFactory::instance();
+    pPredictorFactory.Register("MC", PredictorFactory::Create<MonteCarloPredictor>);
+
+    ProgManager PM = ProgManager("prog.cfg");
     PM.run();
-    
-    log.WriteLine(LOG_INFO, MODULE_NAME, "Finished verificaiton test");
-    
     return 0;
 }
