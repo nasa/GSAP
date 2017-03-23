7fc9bb2311f9ef2385774ee9a6e87b0e2baac940
micah <mricks1@bulldogs.aamu.edu>
2017-03-23 16:26:50 -0700

    TCPServer Class

--- Test/supportTests/TCPTests.cpp
+++ Test/supportTests/TCPTests.cpp
@@ -16,120 +16,102 @@ using namespace PCOE::Test;
 
 std::mutex mtx, mtx2, mtx3;
 
-int openServer(int port) {
+class TCPServer{
+
+public:
+
+
+   TCPServer(int p)
+  {
     struct sockaddr_in server;
-
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    int port=p;
+    int enable=1;
     if (sock == -1) {
         perror("Socket Creation Failed: The following error occurred");
         exit(1);
     }
 
-    int enable = 1;
+
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
         throw "Failed to set SO_REUSEADDR";
     }
 
     bzero((char *) &server, sizeof(server));
 
-    //Prepare the sockaddr_in structure
     server.sin_family=AF_INET;
     server.sin_addr.s_addr= inet_addr("127.0.0.1");
     server.sin_port= htons(port);
+
     //bind
     if(bind(sock, (struct sockaddr *)&server, sizeof(server)) <0) {
         perror("Bind Failed: The following error occurred");
         exit(1);
     }
-
-    return sock;
 }
 
-int serverListen(int server) {
+int Listen1() {
     struct sockaddr_in client;
 
-    listen(server,3);
+    listen(sock,3);
     int c = sizeof(struct sockaddr_in);
-    int sock = accept(server, (struct sockaddr *)&client, (socklen_t*)&c);
-    if (sock<0) {
-        perror("accept failed");
-        exit(1);
+    int client_sock = accept(sock, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock<0) {
+        printf("accept failed \n");
+      //  exit(1);
     }
-    return sock;
+    return client_sock;
 }
+
+private:
+  int sock;
+
+};
 
 void newServer() {
 
   //Create socket
-    int server = openServer(8887);
+
+    TCPServer server(8887);
+
+    //int server = openServer(8887);
 
     mtx.unlock();
   //listen
-    int client = serverListen(server);
+    int client = server.Listen1();
 
-    close(server);
+    //close(server);
     close(client);
-}
+  }
 
 
 void serverReceive() {
     //Create socket
-    int  server = openServer(5556);
-
+    TCPServer server(5556);
     //listen
     mtx2.unlock();
-    int client = serverListen(server);
+    int client = server.Listen1();
 
     char buf[12];
     int n = read(client,buf,12);
-    close(server);
     close(client);
 }
 
 void serverSend() {
     //Create socket
-    int server = openServer(8887);
-
+    TCPServer server(8888);
     //listen
     mtx3.unlock();
-    int client = serverListen(server);
+    int client = server.Listen1();
 
     // Sends response
-    char message[10] = "Hello";
-    int n = write(client,message,5);
+    char message[] = "Hello";
+    //message="hello";
+
+    int n = write(client,message,6);
     mtx3.unlock();
 
     close(client);
-    close(server);
-}
-
-void testNoDelay() {
-  TCPSocket foo;
-  foo.Connect("localhost",630);
-  bool value = foo.NoDelay();
-
-  Assert::AreEqual(0, value);
-  foo.Close();
-  printf("Line no");
-
-}
-
-void testRecieveBufferSize() {
-  TCPSocket foo;
-  foo.Connect("localhost",630);
-  int value =foo.ReceiveBufferSize();
-  Assert::AreNotEqual(0,value);
-
-}
-
-void testSendBufferSize() {
-  TCPSocket foo;
-  TCPSocket::size_type bar;
-  foo.Connect("localhost",630);
-  bar=foo.SendBufferSize();
-  Assert::IsNotNull(bar);
-
 }
 
 void testConnect() {
@@ -157,25 +139,27 @@ void testSend() {
 
      char buffer[]="Hello World";
      int bar= 12;
-     foo.Send(buffer,bar);
+     int y = foo.Send(buffer,bar);
+
+     Assert::AreNotEqual(y,0);
+
+
      first.join();
      foo.Close();
 }
 
+
 void testReceive() {
     mtx3.lock();
     std::thread first(serverSend);
-
     TCPSocket foo;
 
     mtx3.lock();
-    foo.Connect("127.0.0.1",8887);
-
-    char buff[6] = {0};
+    foo.Connect("127.0.0.1",8888);
+    char buff[6]={0};
 
     mtx3.lock();
     foo.Receive(buff,6);
-
     Assert::AreEqual(std::string(buff), std::string("Hello"));
 
     first.join();
