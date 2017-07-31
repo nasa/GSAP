9c77f87f6df796b8c6d3dea573b8bd433162c348
Chris Teubert <christopher.a.teubert@nasa.gov>
2017-07-30 19:31:12 -0700

    Changed  STDIN Timeout to 0, added debug messages

--- framework/src/STDINCommunicator.cpp
+++ framework/src/STDINCommunicator.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
 #include <iostream>
 
 #include "STDINCommunicator.h"
@@ -17,22 +18,23 @@ namespace PCOE {
     STDINCommunicator::STDINCommunicator() {
         log.WriteLine(LOG_INFO, MODULE_NAME, "Configuring");
 
-        timeout.tv_sec = 1;
+        timeout.tv_sec = 0;
         timeout.tv_usec = 0;
+        log.WriteLine(LOG_TRACE, MODULE_NAME, "Configured");
     }
     void STDINCommunicator::poll() {
+        log.WriteLine(LOG_TRACE, MODULE_NAME, "Entering Poll");
         fd_set fds;
         FD_ZERO(&fds);
-        FD_SET(0, &fds);
+        FD_SET(STDIN_FILENO, &fds);
         int rc = select(1, &fds, nullptr, nullptr, &timeout);
         if (rc < 0) {
             perror("select");
-        } else if (rc == 0) {
-        //if (feof(stdin)) {
+        } else if (rc == 0 || (feof(stdin))) {
             log.WriteLine(LOG_TRACE, MODULE_NAME, "Nothing on STDIN");
             // Timeout
         } else {
-            log.WriteLine(LOG_INFO, MODULE_NAME, "Data Received");
+            log.WriteLine(LOG_TRACE, MODULE_NAME, "Data Received");
             setRead();
         }
     }
