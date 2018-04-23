ef5296a785a73c2d127319cf3df744efa131ba60
Jason Watkins <jason-watkins@outlook.com>
2018-04-23 10:44:51 -0700

    Apply clang format to ProgManager

--- framework/src/ProgManager.cpp
+++ framework/src/ProgManager.cpp
@@ -1,42 +1,43 @@
 /**  @file      ProgManager.cpp     Prognostic Manager
-*   @class     ProgManager         Prongostic Manager
-*   @defgroup  GPIC++    Generic Prognostics Infrastructure-C++
-*   @defgroup  Framework Prognostic Framework
-*
-*   @brief     Main class for C++ Generic Prognostic Infrastructure
-*    This class creates the ProgMonitors and Communication Manager.
-*
-*   @author    Chris Teubert
-*   @version   1.1.0
-*
-*   @pre       Prognostic Configuration File and Prognoster Configuration Files
-*
-*   @bug       Large delay for ending other threads
-*
-*      Contact: Chris Teubert (Christopher.a.teubert@nasa.gov)
-*      Created: November 11, 2015
-*
-*   @copyright Copyright (c) 2013-2018 United States Government as represented by
-*     the Administrator of the National Aeronautics and Space Administration.
-*     All Rights Reserved.
-*/
+ *   @class     ProgManager         Prongostic Manager
+ *   @defgroup  GPIC++    Generic Prognostics Infrastructure-C++
+ *   @defgroup  Framework Prognostic Framework
+ *
+ *   @brief     Main class for C++ Generic Prognostic Infrastructure
+ *    This class creates the ProgMonitors and Communication Manager.
+ *
+ *   @author    Chris Teubert
+ *   @version   1.1.0
+ *
+ *   @pre       Prognostic Configuration File and Prognoster Configuration Files
+ *
+ *   @bug       Large delay for ending other threads
+ *
+ *      Contact: Chris Teubert (Christopher.a.teubert@nasa.gov)
+ *      Created: November 11, 2015
+ *
+ *   @copyright Copyright (c) 2013-2018 United States Government as represented by
+ *     the Administrator of the National Aeronautics and Space Administration.
+ *     All Rights Reserved.
+ */
 
+#include <algorithm> // For tolower
 #include <exception>
 #include <iostream>
-#include <vector>
-#include <algorithm>  // For tolower
 #include <string>
+#include <vector>
 
-#include "SharedLib.h"  // for trim
+#include "CommManager.h"
 #include "ProgManager.h"
 #include "PrognoserFactory.h"
-#include "CommManager.h"
+#include "SharedLib.h" // for trim
 
 namespace PCOE {
     /// CONFIGURABLE PARAMETERS
     const std::string PACKAGE_NAME = "C++ Generic Prognostic Infrastructure";
-    const std::string VERSION = "1.1.0";
-    const std::string NOTE = "If you have technical issues with the plugin, "
+    const std::string VERSION      = "1.1.0";
+    const std::string NOTE =
+        "If you have technical issues with the plugin, "
         "please report them by \nemailing Christopher Teubert (christopher.a.teubert@nasa.gov).";
     const std::string MODULE_NAME = "PrognosticManager";
 
@@ -44,15 +45,15 @@ namespace PCOE {
 
     class CommonCommunicator;
 
-    static Log &logger = Log::Instance();
+    static Log& logger = Log::Instance();
 
-    ProgManager::ProgManager() : configValues(), configSet(false), theComm(CommManager::instance()) { }
+    ProgManager::ProgManager()
+        : configValues(), configSet(false), theComm(CommManager::instance()) {}
 
-    ProgManager::ProgManager(const std::string& path) :
-        ProgManager(GSAPConfigMap(path)) { }
+    ProgManager::ProgManager(const std::string& path) : ProgManager(GSAPConfigMap(path)) {}
 
     ProgManager::ProgManager(const GSAPConfigMap& config)
-        : configValues(config), configSet(true), theComm(CommManager::instance()) { }
+        : configValues(config), configSet(true), theComm(CommManager::instance()) {}
 
     void ProgManager::setConfig(const std::string& path) {
         setConfig(GSAPConfigMap(path));
@@ -60,21 +61,17 @@ namespace PCOE {
 
     void ProgManager::setConfig(const GSAPConfigMap& config) {
         configValues = config;
-        configSet = true;
+        configSet    = true;
     }
 
     void ProgManager::run() {
         enable();
 
-        /// Setup Main Loop
         unsigned int counter = 0;
         Cmd ctrl;
 
-        /// Main Loop- Handle controls for prognosers
         while (ctrl.command != STOP) {
             counter++;
-
-            /// Handle Commands
             ctrl = control();
 
             if (ctrl.command == STOP) {
@@ -86,83 +83,85 @@ namespace PCOE {
             else if (ctrl.command == PAUSE) {
                 pause();
             }
-        }  // End while (command != stop)w
+        }
     }
-    
-    void ProgManager::addPrognoser(const std::string & path) {
-        PrognoserFactory &factory = PrognoserFactory::instance();
+
+    void ProgManager::addPrognoser(const std::string& path) {
+        PrognoserFactory& factory = PrognoserFactory::instance();
         prognosers.push_back(factory.Create(path));
     }
-    
+
     void ProgManager::enable() {
         /// Setup Log
         logger.Initialize(PACKAGE_NAME, VERSION, NOTE);
         logger.WriteLine(LOG_INFO, MODULE_NAME, "Enabling");
-        
+
         if (!configSet) {
-            logger.WriteLine(LOG_DEBUG, MODULE_NAME, "No configuration file set - closing progManager");
+            logger.WriteLine(LOG_DEBUG,
+                             MODULE_NAME,
+                             "No configuration file set - closing progManager");
             return;
         }
-        
+
         /// SETUP PROGNOSERS
         logger.WriteLine(LOG_DEBUG, MODULE_NAME, "Setting Up Prognosers");
         if (configValues.includes("Prognosers")) {
-            for (auto & itStrs : configValues.at("Prognosers")) {
+            for (auto& itStrs : configValues.at("Prognosers")) {
                 addPrognoser(itStrs);
                 // @todo(CT): Add check that component was made correctly
             }
         }
-        
+
         /// Setup COMMUNICATION
         // Note: This must be done after the prognosers
         theComm.configure(configValues);
         theComm.start();
-        
+
         logger.WriteLine(LOG_DEBUG, MODULE_NAME, "Enabled");
     }
-    
+
     void ProgManager::start() {
         logger.WriteLine(LOG_INFO, MODULE_NAME, "Starting");
         /// START PROGNOSERS
-        for (auto & prognoser : prognosers) {
+        for (auto& prognoser : prognosers) {
             prognoser->start();
         }
-        
+
         logger.WriteLine(LOG_DEBUG, MODULE_NAME, "Started");
     }
-    
+
     void ProgManager::pause() {
         logger.WriteLine(LOG_INFO, MODULE_NAME, "Pausing");
         /// PAUSE PROGNOSERS
-        for (auto & prognoser : prognosers) {
+        for (auto& prognoser : prognosers) {
             prognoser->pause();
         }
-        
+
         logger.WriteLine(LOG_DEBUG, MODULE_NAME, "Paused");
     }
-    
+
     void ProgManager::stop() {
         logger.WriteLine(LOG_INFO, MODULE_NAME, "Stopping");
         /// STOP PROGNOSERS
-        for (auto & prognoser : prognosers) {
+        for (auto& prognoser : prognosers) {
             prognoser->stop();
         }
-        
+
         logger.WriteLine(LOG_DEBUG, MODULE_NAME, "Cleanup");
-        
+
         /// CLEANUP ACTIVITIES
         // End each Prognoser
-        for (auto & prognoser : prognosers) {
+        for (auto& prognoser : prognosers) {
             logger.WriteLine(LOG_DEBUG, MODULE_NAME, "Waiting for Prognoser thread to stop");
-            prognoser->join();// Wait for thread to end
+            prognoser->join(); // Wait for thread to end
         }
-        
+
         // Stop Communication Manager
         // NOTE: This has to be done after the other threads that used it are stopped
         theComm.stop();
         logger.WriteLine(LOG_DEBUG, MODULE_NAME, "Waiting for Comm thread to stop");
         theComm.join();
-        
+
         // Stop Log, exit thread
         logger.WriteLine(LOG_INFO, MODULE_NAME, "Stopped");
         logger.Close();
@@ -175,7 +174,7 @@ namespace PCOE {
         Cmd c;
 
         std::cout << "prg $ ";
-        std::cin >> input;  // Receive input
+        std::cin >> input; // Receive input
         logger.FormatLine(LOG_TRACE, MODULE_NAME, "Control Command received- %s", input.c_str());
         trim(input);
 
@@ -184,7 +183,7 @@ namespace PCOE {
             return c;
         }
 
-        const auto marker = input.find_first_of(" \t");
+        const auto marker   = input.find_first_of(" \t");
         std::string command = (input.substr(0, marker));
         std::transform(command.begin(), command.end(), command.begin(), ::tolower);
 
