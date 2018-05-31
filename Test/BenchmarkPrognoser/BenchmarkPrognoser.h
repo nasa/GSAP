144b8218ed39b881b689b4215ce27d1f6367b089
Jason Watkins <jason.watkins@nasa.gov>
2018-05-31 10:39:04 -0700

    Fix contact in BenchmarkPrognoser

--- Test/BenchmarkPrognoser/BenchmarkPrognoser.h
+++ Test/BenchmarkPrognoser/BenchmarkPrognoser.h
@@ -1,40 +1,43 @@
 /**  Benchmark Prognoser - Header
-*   @class     BenchmarkPrognoser BenchmarkPrognoser.h
-*
-*   @brief     Benchmark Prognoser Class
-*
-*   BenchmarkPrognoser class.
-*
-*   @author    Micah Ricks
-*   @version   1.1.0
-*
-*
-*      Contact: Micah Ricks (mricks1@bulldogs.aamu.edu)
-*      Created: March 16, 2016
-*
-*   @copyright Copyright (c) 2018 United States Government as represented by
-*     the Administrator of the National Aeronautics and Space Administration.
-*     All Rights Reserved.
-*/
+ *   @class     BenchmarkPrognoser BenchmarkPrognoser.h
+ *
+ *   @brief     Benchmark Prognoser Class
+ *
+ *   BenchmarkPrognoser class.
+ *
+ *   @author    Micah Ricks
+ *   @version   1.1.0
+ *
+ *
+ *      Contact: Christopher Teubert (christopher.a.teubert@nasa.gov)
+ *      Created: March 16, 2016
+ *
+ *   @copyright Copyright (c) 2018 United States Government as represented by
+ *     the Administrator of the National Aeronautics and Space Administration.
+ *     All Rights Reserved.
+ */
 #ifndef PCOE_BENCHMARKPROGNOSER_H
 #define PCOE_BENCHMARKPROGNOSER_H
 
+#include "BenchmarkTimer.h"
 #include "ModelBasedPrognoser.h"
-#include "PrognosticsModel.h"
 #include "Observer.h"
 #include "Predictor.h"
-#include "BenchmarkTimer.h"
+#include "PrognosticsModel.h"
+
 namespace PCOE {
-  class BenchmarkPrognoser : public ModelBasedPrognoser {
-   private:
+    class BenchmarkPrognoser : public ModelBasedPrognoser {
+    private:
         BenchmarkTimer benchmark1;
-   public:
-        /** @brief      Benchmark Prognoser Constructor
-         *  @param      config Map of config parameters from the prognoser config file
+
+    public:
+        /**
+         * @brief Benchmark Prognoser Constructor
+         * @param config Map of config parameters from the prognoser config file
          */
-        BenchmarkPrognoser(GSAPConfigMap & config);
-        ~BenchmarkPrognoser();  // destructor
+        BenchmarkPrognoser(GSAPConfigMap& config);
+        ~BenchmarkPrognoser(); // destructor
         void step();
-  };
+    };
 }
-#endif  // PCOE_BENCHMARKPROGNOSER_H
+#endif // PCOE_BENCHMARKPROGNOSER_H
