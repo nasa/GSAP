62ff4c47cd4342c4fdec9d3ec8b00f1d0ee937f3
Jason Watkins <jason.watkins@nasa.gov>
2018-05-31 10:27:41 -0700

    Some cleanup of GaussianVariable

--- support/inc/GaussianVariable.h
+++ support/inc/GaussianVariable.h
@@ -1,6 +1,6 @@
 /* @author    Shankar Sankararaman
  *
- * @copyright Copyright (c) 2018 United States Government as represented by
+ * @copyright Copyright (c) 2016 - 2018 United States Government as represented by
  *            the Administrator of the National Aeronautics and Space
  *            Administration. All Rights Reserved.
  */
@@ -9,32 +9,26 @@
 #define PCOE_GAUSSIANVARIABLE_H
 
 namespace PCOE {
-    class GaussianVariable    // MD: Derive from some abstract class? Common functions would be evaluatepdf/cdf, invertcdf, setparameters, generatesample(s)
-    {
-    private:
-        double mu, sigma; /* mean, standard deviation, variable instance, uniform random variable*/
-
+    class GaussianVariable {
     public:
-        //double X, U;
-        double VarSamples[10000];    // MD: Change to vector, don't hardcode size
-        GaussianVariable();  // Constructor 1
-        GaussianVariable(double, double);  // Constructor 2 - set mean and standard deviation
+        // double X, U;
+        double VarSamples[10000]; // MD: Change to vector, don't hardcode size
+        GaussianVariable(); // Constructor 1
+        GaussianVariable(double, double); // Constructor 2 - set mean and standard deviation
 
         double evaluatepdf(double); /* variable instance, mean, standard deviation */
         double evaluatecdf(double); /* variable instance, mean, standard deviation */
-        double invertcdfur(double);  /* uniform random variable, mean, standard deviation */
+        double invertcdfur(double); /* uniform random variable, mean, standard deviation */
         void setmeanstd(double, double); /* to store the values of mean and standard deviation*/
-        void generatesamplesdirect(int); /*generate a particular number of samples using diret c++ random generator*/
-        void generatesamplesicdfur(int); /*generate a particular number of samples using cdf inversion*/
+        void generatesamplesdirect(
+            int); /*generate a particular number of samples using diret c++ random generator*/
+        void
+        generatesamplesicdfur(int); /*generate a particular number of samples using cdf inversion*/
+
+    private:
+        double mu;
+        double sigma;
     };
 }
 
 #endif // PCOE_GAUSSIANVARIABLE_H
-
-
-
-
-
-
-
-
