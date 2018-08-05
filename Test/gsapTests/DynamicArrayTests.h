03da8122cf3772bf8235bc4f67d9040eeda51994
Jason Watkins <jason.watkins@nasa.gov>
2018-08-05 10:03:04 -0700

    Dynamic Array improvements

---
+++ Test/gsapTests/DynamicArrayTests.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2018 United States Government as represented by the
+// Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+#ifndef DYNAMICARRAYTESTS_H
+#define DYNAMICARRAYTESTS_H
+#include "Test.h"
+
+using namespace PCOE::Test;
+
+namespace DynamicArrayTests {
+    void registerTests(TestContext& context);
+}
+
+#endif
