9580ba3a611f092569371875a25bdbdea9a7db94
Jason Watkins <jason.watkins@nasa.gov>
2018-08-27 14:13:05 -0700

    Add logging to EventDrivenObserver

--- src/Observers/EventDrivenObserver.cpp
+++ src/Observers/EventDrivenObserver.cpp
@@ -6,6 +6,9 @@
 #include "Messages/UDataMessage.h"
 
 namespace PCOE {
+    static const Log& log = Log::Instance();
+    static const std::string MODULE_NAME = "OBS-ED";
+
     EventDrivenObserver::EventDrivenObserver(MessageBus& messageBus,
                                              std::unique_ptr<Observer>&& obs,
                                              std::string src)
@@ -30,20 +33,23 @@ namespace PCOE {
 
     void EventDrivenObserver::processMessage(const std::shared_ptr<Message>& message) {
         // Note (JW): If we are unable to aquire the lock immediately, the
-        //            observer is alreadyu processing a message. If we can't
+        //            observer is already processing a message. If we can't
         //            aquire the lock within a few milliseconds, the observer is
         //            probably producing a state estimate, and the message queue
         //            is backed up.
         unique_lock lock(m, std::chrono::milliseconds(10));
         if (!lock.owns_lock()) {
+            log.WriteLine(LOG_DEBUG, MODULE_NAME, "Skipping observer step. Failed to aquire lock.");
             return;
         }
 
         switch (message->getMessageId()) {
         case MessageId::ModelInputVector:
+            log.WriteLine(LOG_TRACE, MODULE_NAME, "Set input message");
             inputMsg = message;
             break;
         case MessageId::ModelOutputVector:
+            log.WriteLine(LOG_TRACE, MODULE_NAME, "Set ouput message");
             outputMsg = message;
             break;
         default:
@@ -57,11 +63,14 @@ namespace PCOE {
             const auto& u = Model::input_type(typedInMsg->getValue());
             const auto& z = Model::output_type(typedOutMsg->getValue());
             if (!observer->isInitialized()) {
+                log.WriteLine(LOG_TRACE, MODULE_NAME, "Initialized observer");
                 auto x = observer->getModel().initialize(u, z);
                 observer->initialize(latestTimestamp, x, u);
             }
             else {
+                log.WriteLine(LOG_TRACE, MODULE_NAME, "Stepping observer");
                 observer->step(latestTimestamp, u, z);
+                log.WriteLine(LOG_TRACE, MODULE_NAME, "Publishing observer result");
                 UDataVecMessage* stateEst = new UDataVecMessage(MessageId::ModelStateEstimate,
                                                                 source,
                                                                 observer->getStateEstimate());
