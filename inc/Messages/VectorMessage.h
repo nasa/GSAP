564e073ade10798a141d7d2ddeb1dd1c64d72f03
Jason Watkins <jason.watkins@nasa.gov>
2018-09-07 13:04:16 -0700

    Fix vector message

--- inc/Messages/VectorMessage.h
+++ inc/Messages/VectorMessage.h
@@ -42,20 +42,6 @@ namespace PCOE {
          * Constructs a new instance of @{code VectorMessage}, moving the
          * contents of {@param values}.
          *
-         * @param id     The id of the message.
-         * @param source The source of the message.
-         * @param value  The value of the message.
-         **/
-        VectorMessage(MessageId id, std::string source, std::vector<T>&& values)
-            : Message(id, source), values(values) {
-            Expect((static_cast<std::uint64_t>(id) & 0x0000C00000000000L) > 0,
-                   "Message id is not vector");
-        }
-
-        /**
-         * Constructs a new instance of @{code VectorMessage}, moving the
-         * contents of {@param values}.
-         *
          * @param id        The id of the message.
          * @param source    The source of the message.
          * @param timestamp The time at which the message or the data contained
