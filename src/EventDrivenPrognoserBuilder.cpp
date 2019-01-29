3445e8b41563ab7e89023df7178c8a481d5be69a
Jason Watkins <jason.watkins@nasa.gov>
2019-01-29 11:07:35 -0800

    Fix missing header

--- src/EventDrivenPrognoserBuilder.cpp
+++ src/EventDrivenPrognoserBuilder.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2018 United States Government as represented by the
 // Administrator of the National Aeronautics and Space Administration.
 // All Rights Reserved.
-
+#include <memory>
 #include <sstream>
 
 #include "ConfigMap.h"
