6ca227319106bad892d36121ca4a6982eb5d1a73
Christopher Teubert <christopher.a.teubert@nasa.gov>
2019-11-20 15:46:13 -0800

    Organized benchmark directory

--- benchmarking/src/main.cpp
+++ benchmarking/src/main.cpp
@@ -1,38 +1,110 @@
-//
-//  progMain.cpp
-//  Example
-//
-//  Created by Teubert, Christopher (ARC-TI)[SGT, INC] on 5/4/16.
-//  Copyright © 2018 United States Government as represented by the Administrator of the National
-//  Aeronautics and Space Administration.  All Rights Reserved.
-//
-
-#include "BenchmarkPrognoser.h"
-#include "PrognoserFactory.h"
-
-#include "ConfigMap.h"
-#include <ctime>
+// TODO (JW): By its nature, this file is designed to be copied. It should
+// be explicitely (un)licensed as public domain or CC0.
+#include <chrono>
 #include <fstream>
-#include <iostream>
-//#include "BenchmarkTest.cpp"
+#include <map>
+#include <sstream>
+#include <vector>
+
+#include "PrognoserFactory.h"
 
 using namespace PCOE;
 
+// This function is a quick and dirty reader for the example data files.
+// For production-ready applications, a complete and well-tested CSV library
+// should be used.
+std::vector<std::map<MessageId, Datum<double>>> read_file(const std::string& filename) {
+    using namespace std::chrono;
+
+    std::ifstream file(filename);
+    if (file.fail()) {
+        std::cerr << "Unable to open data file" << std::endl;
+    }
+    // Skip header line
+    file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    auto now = system_clock::now();
+
+    std::vector<std::map<MessageId, Datum<double>>> result;
+    while (file.good()) {
+        std::map<MessageId, Datum<double>> data;
+        std::string line;
+        std::getline(file, line);
+        if (line.empty()) {
+            continue;
+        }
+        std::stringstream line_stream(line);
+        std::string cell;
+        std::getline(line_stream, cell, ',');
+        double file_time = std::stod(cell);
+        auto timestamp = now + milliseconds(static_cast<unsigned>(file_time * 1000));
+
+        std::getline(line_stream, cell, ',');
+        Datum<double> power(std::stod(cell));
+        power.setTime(timestamp);
+
+        std::getline(line_stream, cell, ',');
+        Datum<double> temperature(std::stod(cell));
+        temperature.setTime(timestamp);
+
+        std::getline(line_stream, cell, ',');
+        Datum<double> voltage(std::stod(cell));
+        voltage.setTime(timestamp);
+
+        data.insert({MessageId::Watts, power});
+        data.insert({MessageId::Centigrade, temperature});
+        data.insert({MessageId::Volts, voltage});
+        result.push_back(data);
+    }
+    return result;
+}
+
+// This example sets up a simple synchronos prognoser to predict battery end of
+// discharge using a Monte Carlo predictor and an Unscented Kalman Filter.
 int main() {
-    ConfigMap::addSearchPath("../example/cfg/");
+    using namespace std::chrono;
+    // Read battery data from a file.
+    auto data = read_file("data/data_const_load.csv");
 
-    // Specify Prognosers
-    PrognoserFactory& prognoserFactory = PrognoserFactory::instance();
-    prognoserFactory.Register<BenchmarkPrognoser>("BenchmarkPrognoser");
+    // Create a configuration from a file
+    ConfigMap config("cfg/example_simple.cfg");
 
-    ProgManager PM = ProgManager("bench.cfg");
+    // Contruct a new prognoser using the prognoser factory. The prognoser
+    // will automatically construct an appropriate model, observer and predictor
+    // based on the values specified in the config.
+    auto prognoser = PrognoserFactory::instance().Create("ModelBasedPrognoser", config);
 
-    printf("Benchmarking\n");
-    PM.enable();
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    PM.start();
-    std::this_thread::sleep_for(std::chrono::minutes(1));
-    PM.stop();
+    // For each line of data in the example file, run a single prediction step.
+    for (const auto& line : data) {
+        // Get a new prediction
+        auto prediction = prognoser->step(line);
+
+        // Get the event for battery EoD. The first line of data is used to initialize the observer,
+        // so the first prediction won't have any events.
+        if (prediction.getEvents().size() == 0) {
+            continue;
+        }
+        auto eod_event = prediction.getEvents().front();
+
+        // The time of event is a `UData` structure, which represents a data
+        // point while maintaining uncertainty. For the MonteCarlo predictor
+        // used by this example, the uncertainty is captured by storing the
+        // result of each particle used in the prediction.
+        UData eod_time = eod_event.getTOE();
+        if (eod_time.uncertainty() != UType::Samples) {
+            std::cerr << "Unexpected uncertainty type for EoD prediction" << std::endl;
+            return 1;
+        }
+
+        // For this example, we will print the median EoD.
+        auto samples = eod_time.getVec();
+        std::sort(samples.begin(), samples.end());
+        double eod_median = samples.at(samples.size() / 2);
+        auto now =  MessageClock::now();
+        auto now_s = duration_cast<std::chrono::seconds>(now.time_since_epoch());
+        std::cout << "Predicted median EoD: " << eod_median << " s (T- "
+                  << (eod_median-now_s.count())<< " s)" << std::endl;
+    }
 
     return 0;
 }
