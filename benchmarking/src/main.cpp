bf03d65eae27683b6b08a8c2a252bf186a44f331
Christopher Teubert <christopher.a.teubert@nasa.gov>
2019-11-26 10:03:25 -0800

    Fixes

--- benchmarking/src/main.cpp
+++ benchmarking/src/main.cpp
@@ -9,6 +9,7 @@
 #include "PrognoserFactory.h"
 
 using namespace PCOE;
+long start_s;
 
 // This function is a quick and dirty reader for the example data files.
 // For production-ready applications, a complete and well-tested CSV library
@@ -24,6 +25,7 @@ std::vector<std::map<MessageId, Datum<double>>> read_file(const std::string& fil
     file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     auto now = system_clock::now();
+    start_s = duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
 
     std::vector<std::map<MessageId, Datum<double>>> result;
     while (file.good()) {
@@ -37,19 +39,19 @@ std::vector<std::map<MessageId, Datum<double>>> read_file(const std::string& fil
         std::string cell;
         std::getline(line_stream, cell, ',');
         double file_time = std::stod(cell);
-        auto timestamp = now + milliseconds(static_cast<unsigned>(file_time * 1000));
+        auto last_timestamp = now + milliseconds(static_cast<unsigned>(file_time * 1000));
 
         std::getline(line_stream, cell, ',');
         Datum<double> power(std::stod(cell));
-        power.setTime(timestamp);
+        power.setTime(last_timestamp);
 
         std::getline(line_stream, cell, ',');
         Datum<double> temperature(std::stod(cell));
-        temperature.setTime(timestamp);
+        temperature.setTime(last_timestamp);
 
         std::getline(line_stream, cell, ',');
         Datum<double> voltage(std::stod(cell));
-        voltage.setTime(timestamp);
+        voltage.setTime(last_timestamp);
 
         data.insert({MessageId::Watts, power});
         data.insert({MessageId::Centigrade, temperature});
@@ -62,13 +64,19 @@ std::vector<std::map<MessageId, Datum<double>>> read_file(const std::string& fil
 // This example sets up a simple synchronos prognoser to predict battery end of
 // discharge using a Monte Carlo predictor and an Unscented Kalman Filter.
 int main() {
-    using namespace std::chrono;
     // Read battery data from a file.
     auto data = read_file("data/data_const_load.csv");
 
     // Create a configuration from a file
     ConfigMap config("cfg/example_simple.cfg");
 
+    // Prepare output file
+    std::ofstream file("simple_performance.csv");
+    if (file.fail()) {
+        std::cerr << "Unable to open data file" << std::endl;
+    }
+    file << "Mission Time (s), " <<  "RUL -2σ, " << "RUL -1σ, " <<  "RUL 𝝁, "  << "RUL +1σ, " << "RUL +2σ," << "True RUL" << std::endl;
+    
     // Contruct a new prognoser using the prognoser factory. The prognoser
     // will automatically construct an appropriate model, observer and predictor
     // based on the values specified in the config.
@@ -76,6 +84,9 @@ int main() {
 
     // For each line of data in the example file, run a single prediction step.
     for (const auto& line : data) {
+        static int counter = 0;
+        counter++;
+        
         // Get a new prediction
         auto prediction = prognoser->step(line);
 
@@ -91,19 +102,17 @@ int main() {
         // used by this example, the uncertainty is captured by storing the
         // result of each particle used in the prediction.
         UData eod_time = eod_event.getTOE();
-        if (eod_time.uncertainty() != UType::Samples) {
-            std::cerr << "Unexpected uncertainty type for EoD prediction" << std::endl;
-            return 1;
-        }
 
         // For this example, we will print the median EoD.
         auto samples = eod_time.getVec();
         std::sort(samples.begin(), samples.end());
-        double eod_median = samples.at(samples.size() / 2);
-        auto now =  MessageClock::now();
-        auto now_s = duration_cast<std::chrono::seconds>(now.time_since_epoch());
-        std::cout << "Predicted median EoD: " << eod_median << " s (T- "
-                  << (eod_median-now_s.count())<< " s)" << std::endl;
+        double eod_median = samples.at(samples.size() / 2) - counter-start_s;
+        double eod_1sd = samples.at(samples.size() * 0.841) - counter-start_s;
+        double eod_m1sd = samples.at(samples.size() * 0.159) - counter-start_s;
+        double eod_2sd = samples.at(samples.size() * 0.977) - counter-start_s;
+        double eod_m2sd = samples.at(samples.size() * 0.023) - counter-start_s;
+        
+        file << counter << ',' << eod_m2sd << ',' << eod_m1sd << ','  << eod_median << ',' << eod_1sd << ',' << eod_2sd << ',' << 2820-counter << std::endl;
     }
 
     return 0;
