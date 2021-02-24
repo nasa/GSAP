c20f2019f4d8d21e54af2129bd77ed8034133867
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-02-24 10:01:34 -0800

    Fixed bug in tests

--- tests/src/Loading/LoadEstimatorTests.cpp
+++ tests/src/Loading/LoadEstimatorTests.cpp
@@ -17,6 +17,6 @@ namespace LoadEstimatorTests {
         registerConstLoadEstimatorTests(context);
         registerGaussianLoadEstimatorTests(context);
         registerMovingAverageLoadEstimatorTests(context);
-        registerProfileLoadEstimatorTests(context);
+        registerConstLoadEstimatorTests(context);
     }
 }
